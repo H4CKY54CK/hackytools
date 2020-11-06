669759c398e5a2729326fd1cf0b7cfd76ff2599c
Hackysack <tk13xr37@gmail.com>
2020-11-06 18:59:43 -0500

    Update cli.py

--- hackytools/cli.py
+++ hackytools/cli.py
@@ -5,16 +5,22 @@ import sys
 
 def hackystats(args):
 
-    cpu_path = lambda i: f"/sys/devices/system/cpu/cpu{i}/cpufreq/scaling_cur_freq"
+    cpu_cur = lambda i: f"/sys/devices/system/cpu/cpu{i}/cpufreq/scaling_cur_freq"
+    cpu_max = lambda i: f"/sys/devices/system/cpu/cpu{i}/cpufreq/scaling_max_freq"
     temps = "/sys/class/thermal/thermal_zone0/temp"
 
     x = os.cpu_count()
 
     print(f"\x1b[4mCPU Freqs ({x})\x1b[0m")
     for i in range(x):
-        with open(cpu_path(i)) as f:
+        with open(cpu_cur(i)) as f:
             freq = int(f.read().strip())
-        print(f"  CPU{i}: {freq/1000:,.0f} MHz")
+        with open(cpu_max(i)) as f:
+            max_freq = int(f.read().strip())
+        if freq > 1000000:
+            print(f"  CPU{i}: {freq/1000000:,.2f} GHz (max: {max_freq/1000000:,.2f} GHz)")
+        else:
+            print(f"  CPU{i}: {freq/1000:,.1f} MHz (max: {max_freq/1000:,.1f} MHz)")
 
     print(f"\x1b[4mTemperature\x1b[0m")
     with open(temps) as f:
