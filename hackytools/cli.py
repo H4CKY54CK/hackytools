636d16cf6a13086eaf57ad5ac472458f85c8c07c
Hackysack <tk13xr37@gmail.com>
2020-11-06 22:03:01 -0500

    top underline, rest bold

--- hackytools/cli.py
+++ hackytools/cli.py
@@ -5,8 +5,8 @@ import platform
 
 def hackystats(args):
 
-    sys.stdout.write(f"\x1b[1m{platform.platform()}\x1b[0m\n\n")
-    sys.stdout.write(f"\x1b[4mCPU Freqs (0-{os.cpu_count() - 1}):\x1b[0m\n")
+    sys.stdout.write(f"\x1b[4m{platform.platform()}\x1b[0m\n\n")
+    sys.stdout.write(f"\x1b[1mCPU Freqs (0-{os.cpu_count() - 1}):\x1b[0m\n")
     for i in range(os.cpu_count()):
         with open(f"/sys/devices/system/cpu/cpu{i}/cpufreq/scaling_cur_freq") as f:
             freq = int(f.read().strip())
@@ -15,7 +15,7 @@ def hackystats(args):
         else:
             sys.stdout.write(f"  CPU {i}: {freq/1000:.2f} GHz\n")
 
-    sys.stdout.write("\x1b[4mTemperature\x1b[0m:\n")
+    sys.stdout.write("\x1b[1mTemperature\x1b[0m:\n")
     with open("/sys/class/thermal/thermal_zone0/temp") as f:
         t = int(f.read().strip()) / 1000
     sys.stdout.write(f"  {t:.1f} \u00b0C | {t*9/5+32:.0f} \u00b0F\n")
