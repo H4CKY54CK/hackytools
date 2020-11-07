22d9605cb4d10785df81ac10df05aade6de16697
Hackysack <tk13xr37@gmail.com>
2020-11-06 19:22:31 -0500

    Update cli.py

--- hackytools/cli.py
+++ hackytools/cli.py
@@ -1,9 +1,33 @@
 import os
 import argparse
 import sys
+sprint = sys.stdout.write
 
+
+
+def hackystats_g(args):
+    data = {
+        'freqs': [f"/sys/devices/system/cpu/cpu{i}/cpufreq/scaling_cur_freq" for i in range(os.cpu_count())],
+        'temp': "/sys/class/thermal/thermal_zone0/temp",
+    }
+
+    for key in data:
+        if key == 'freqs':
+            sprint(f"\x1b[1mCPU Freqs ({len(data[key])} cores)\x1b[0m\n")
+            for item in data[key]:
+                with open(item) as f:
+                    freq = int(f.read().strip())
+                sprint(f"  {freq/1000:.0f} MHz (core {data[key].index(item)})\n")
+        if key == 'temp':
+            with open(data[key]) as f:
+                t = int(f.read().strip())
+            tc = t/1000
+            tf = tc * 9 / 5 + 32
+            sprint(f"Temperature: {tc:.1f} \u00b0C | {tf:.1f} \u00b0F\n")
 
 def hackystats(args):
+    if args.generator:
+        return hackystats_g(args)
 
     cpu_cur = lambda i: f"/sys/devices/system/cpu/cpu{i}/cpufreq/scaling_cur_freq"
     cpu_max = lambda i: f"/sys/devices/system/cpu/cpu{i}/cpufreq/scaling_max_freq"
@@ -37,6 +61,7 @@ def main(argv=None):
     argv = (argv or sys.argv)[1:]
     parser = argparse.ArgumentParser()
     parser.add_argument('--fahrenheit', '-f', action='store_true', help="show temperature in fahrenheit instead of celsius")
+    parser.add_argument('--generator', '-g', action='store_true')
     parser.set_defaults(func=hackystats)
     args = parser.parse_args(argv)
     return args.func(args)
