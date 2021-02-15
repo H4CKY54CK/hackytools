4874c20f270e6687a3bb683528f86a737399a493
Scot_Survivor <unconfigured@null.spigotmc.org>
2021-02-15 09:46:39 +0000

    spriteit.o

--- hackytools/spriteit.py
+++ hackytools/spriteit.py
@@ -6,7 +6,6 @@ from math import sqrt, ceil, floor
 from PIL import Image
 from threading import Thread
 
-
 class Sprite:
 
     def __init__(self, args):
@@ -37,18 +36,15 @@ class Sprite:
         threads = []
         for item in self.dirs:
             t1 = Thread(target=self.single_sheet, args=(item, self.width, self.height, self.output))
-            t2 = Thread(target=self.generate_stylesheet, args=(item, self.width, self.height, self.output),
-                        kwargs={'project': p})
-            threads.extend((t1, t2))
+            t2 = Thread(target=self.generate_stylesheet, args=(item, self.width, self.height, self.output), kwargs={'project': p})
+            threads.extend((t1,t2))
         for thread in threads:
             thread.start()
         for thread in threads:
             thread.join()
         print(f"{self.stylesheet_file} generated")
 
-    # Static method, the method doesn't call self. no need for it to be a self method
-    @staticmethod
-    def single_sheet(source, width, height, output):
+    def single_sheet(self, source, width, height, output):
 
         images = [file.path for file in os.scandir(source)]
         if len(images) > ceil(sqrt(len(images))) * floor(sqrt(len(images))):
@@ -56,39 +52,37 @@ class Sprite:
         else:
             rows, cols = ceil(sqrt(len(images))), floor(sqrt(len(images)))
         size = Image.open(images[0]).size
-        canvas = (size[0] * rows, size[1] * cols)
+        canvas = (size[0]*rows,size[1]*cols)
         x = y = 0
         sheet = Image.new('RGBA', canvas)
         for item in images:
             if os.path.isdir(item):
                 continue
-            sheet.paste(Image.open(item), (x, y))
+            sheet.paste(Image.open(item), (x,y))
             x += size[0]
             if x >= canvas[0]:
                 x = 0
                 y += size[1]
         if width and height:
-            sheet = sheet.resize((width * rows, height * cols), Image.LANCZOS)
+            sheet = sheet.resize((width*rows, height*cols), Image.LANCZOS)
         # if not os.path.exists(output):
-        # os.mkdir(output)
+            # os.mkdir(output)
         filename = os.path.join(output, 'flairs-{}.png'.format(os.path.split(source)[1]))
-        sheet.save(filename, 'PNG')
-        print("{} generated.".format(filename.replace('.png', '')))
+        sheet.save(filename,'PNG')
+        print(f"{filename} generated.")
 
-    # Static method, the method doesn't call self. no need for it to be a self method
-    @staticmethod
-    def generate_stylesheet(source, width, height, output, project=None):
+    def generate_stylesheet(self, source, width, height, output, project=None):
 
         images = [file for file in os.scandir(source)]
         size = Image.open(images[0].path).size
         if width and height:
-            size = (width, height)
+            size = (width,height)
         x = y = 0
         if len(images) > ceil(sqrt(len(images))) * floor(sqrt(len(images))):
             rows, cols = ceil(sqrt(len(images))), ceil(sqrt(len(images)))
         else:
             rows, cols = ceil(sqrt(len(images))), floor(sqrt(len(images)))
-        stylesheet = os.path.join(output, 'stylesheet.css')
+        stylesheet = os.path.join(output,'stylesheet.css')
         lines = []
         folder = ''
         if project:
@@ -108,23 +102,10 @@ class Sprite:
             f.writelines(lines)
         self.stylesheet_file = stylesheet
 
-def start(args):
-    Sprite(args).spriteit()
+def spriteit(args):
 
-
-def main(argv=None):
-    argv = (argv or sys.argv)[1:]
-    parser = argparse.ArgumentParser()
-    parser.add_argument('source')
-    parser.add_argument('output', nargs='?', default='sprites')
-    parser.add_argument('-x', '--width', dest='width', type=int, default=None)
-    parser.add_argument('-y', '--height', dest='height', type=int, default=None)
-    parser.add_argument('-xy', '--size', dest='size', type=int, default=None)
-    # parser.add_argument('-V', '--version', action='store_true')
-    parser.set_defaults(func=start)
-    args = parser.parse_args(argv)
     if not args.width and not args.height and args.size:
         args.width = args.height = args.size
 
-if __name__ == '__main__':
-    sys.exit(main())
+    Sprite(args).spriteit()
+
